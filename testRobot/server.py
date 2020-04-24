37c156e43b7020a2b4ac9a67c6592cf19af08172
Popescu Aura <popescuauraloredana@gmail.com>
2020-04-24 19:55:44 +0300

    client-server for bluetooth

---
+++ testRobot/server.py
@@ -0,0 +1,46 @@
+import bluetooth
+from alpha1p import client
+
+
+def main():
+    msg = message(b'\x18', [b'\x00'])
+    print(msg)
+    bd_addr = discover()
+
+    if bd_addr:
+        port = 6
+        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        sock.connect((bd_addr, port))
+        print('Connected')
+        sock.settimeout(60.0)
+        sock.send(msg)
+        print('Sent data')
+        response = sock.recv(1024)
+        print(client().get(response))
+        sock.close()
+
+
+def message(command, parameters):
+    header = b'\xFB\xBF'
+    end = b'\xED'
+    parameter = b''.join(parameters)
+    # len(header + length + command +parameters + check)
+    length = bytes([len(parameters) + 5])
+    data = [command, length]
+    data.extend(parameters)
+    check = bytes([sum(ord(x) for x in data)])
+    return header+length+command+parameter+check+end
+
+
+def discover():
+    print("searching ...")
+    nearby_devices = bluetooth.discover_devices(lookup_names=True)
+    print("found %d devices" % len(nearby_devices))
+
+    for addr, name in nearby_devices:
+        if name == "Alpha1_230F":
+            return addr
+
+
+if __name__ == '__main__':
+    main()
